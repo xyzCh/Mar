75fb1018cbc47a08e54fec9619363dc8cca23a7c
xyzch <hsn940814@live.com>
2019-07-11 17:05:37 +0800

    commit

--- a.c
+++ a.c
@@ -1,6 +1,32 @@
 #include <stdio.h>
 
+volatile int count=0;
+void child();
+void parent();
+
 int main(){
-	printf("%s\n","hello world");
-	return 0;
+	switch(fork()){
+		case -1:
+			printf("fork error!\n");
+			break;
+		case 0:
+			child();
+			break;
+		default:
+			parent();
+	}
+}
+
+void child(){
+	while(count<=5){
+		count++;
+		printf("child:%d\n",count);
+	}
+}
+
+void parent(){
+	while (count<=5){
+		count++;
+		printf("parent:%d\n",count);
+	}
 }
